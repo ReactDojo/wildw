4f880af0610bcf8bcaf0d32e20d0cf37d5bbc4f3
Chad Garrett <chadgarrett_@hotmail.com>
2018-05-14 13:13:01 -0700

    Added function to gather location after user logs in

--- src/view/start/home.js
+++ src/view/start/home.js
@@ -1,49 +1,71 @@
 import React, { Component } from 'react';
-var { View, Text, AsyncStorage } = require('react-native');
+import { View, Text, AsyncStorage } from 'react-native';
+import { Permissions, Location } from 'expo';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import { Button } from './index'; //Import your Button
-import styles from '../../styles/auth/index' //Import your styles
-import { fetchOffer, fetchCategory, fetchUserQRCode, fetchAvailableOffers, fetchGetHistory, fetchAllStore } from '../../redux/actions/OfferActions';
+import { Button } from './index';
+import styles from '../../styles/auth/index'
+import { fetchOffer, fetchCategory, fetchUserQRCode, fetchAvailableOffers, fetchGetHistory, fetchAllStore, postUserLocation } from '../../redux/actions/OfferActions';
 import * as Progress from 'react-native-progress';
 
 
 class Home extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    var _this = this;
+    this.props.fetchOffer();
+    this.props.fetchCategory();
+    this.props.fetchGetHistory();
+    this.props.fetchUserQRCode();
+    this.props.fetchAvailableOffers();
+    this.props.fetchAllStore();
+    this._getLocationAsync((location) => {
+      this.props.postUserLocation(location);
+    });
+
+    AsyncStorage.getItem('token', (err, token) => {
+      if (token === null) Actions.welcome();
+      else Actions.offerlist();
+    });
+  }
+
+  _getLocationAsync = async (cb) => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
     }
 
-    componentWillMount() {
-        var _this = this;
-        this.props.fetchOffer();
-        this.props.fetchCategory();
-        this.props.fetchGetHistory();
-        this.props.fetchUserQRCode();
-        this.props.fetchAvailableOffers();
-        this.props.fetchAllStore();
-        
-        AsyncStorage.getItem('token', (err, token) => {
-            if (token === null) Actions.welcome();
-            else Actions.offerlist();
-        });
-    }
-    
-    render() {
-        return (
-            <View style={styles.container}>
-                {
-                    <View>
-                        <Progress.Circle size={30} indeterminate={true} style={{ flex: 1, alignSelf: 'center', justifyContent: 'center' }} />
-                    </View>
-                }
-            </View>
-        );
-    }
+    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    let address = await Location.reverseGeocodeAsync(location.coords);
+    let addressObj = { 
+      address: address[0].name + ', ' + address[0].city + ', ' + address[0].region + ' ' + address[0].postalCode + ', ' + address[0].country,
+      log_type: 'USER_LOGIN_LOCATION' 
+    };
+    let locationObj = Object.assign(addressObj, location);
+    cb(locationObj);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {
+          <View>
+            <Progress.Circle size={30} indeterminate={true} style={{ flex: 1, alignSelf: 'center', justifyContent: 'center' }} />
+          </View>
+        }
+      </View>
+    );
+  }
 };
 
 
 
-export default connect(null, 
-    { fetchOffer, fetchCategory, fetchUserQRCode, fetchAvailableOffers, fetchGetHistory, fetchAllStore }
+export default connect(null,
+  { fetchOffer, fetchCategory, fetchUserQRCode, fetchAvailableOffers, fetchGetHistory, fetchAllStore, postUserLocation }
 )(Home);
